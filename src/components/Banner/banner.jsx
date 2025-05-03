ae221159b496bebc094c1bd75e9139d75dbf1766
Han Frisonsova <hanafrisonsova@gmail.com>
2025-05-03 14:17:39 +0200

    opraví názvy komponent a přidá komponentu drink, kterou zobrazí v menu

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,4 +1,4 @@
-import './banner.css';
+import './Banner.css';
 
 export const Banner = () => {
   return (
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import { render } from '@czechitas/render';
-import { Header } from '../components/Header/header';
-import { Banner } from '../components/Banner/banner';
-import { Menu } from '../components/Menu/menu';
-import { Gallery } from '../components/Gallery/gallery';
-import { Contact } from '../components/Contact/contact';
-import { Footer } from '../components/Footer/footer';
+import { Header } from '../components/Header/Header';
+import { Banner } from '../components/Banner/Banner';
+import { Menu } from '../components/Menu/Menu';
+import { Gallery } from '../components/Gallery/Gallery';
+import { Contact } from '../components/Contact/Contact';
+import { Footer } from '../components/Footer/Footer';
+import { Drink } from '../components/Drink/Drink';
 import '../global.css';
 import './index.css';
 
@@ -21,10 +22,10 @@ document.querySelector('#root').innerHTML = render(
   </div>,
 );
 
-const rolloutElement = document.querySelector('.nav-btn');
+const navToogle = document.querySelector('.nav-btn');
 const rolloutNav = document.querySelector('.rollout-nav');
 
-rolloutElement.addEventListener('click', () => {
+navToogle.addEventListener('click', () => {
   rolloutNav.classList.toggle('nav-closed');
 });
 
